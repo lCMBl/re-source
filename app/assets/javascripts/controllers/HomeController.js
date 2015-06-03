8f8a6a043decfeb0e0b31849fca073207fde492e
Nathan Werbeckes <werbeckes@gmail.com>
2015-06-03 18:20:29 -0500

    debugger?

--- app/assets/javascripts/controllers/HomeController.js
+++ app/assets/javascripts/controllers/HomeController.js
@@ -7,31 +7,32 @@ app.controller("HomeController", [
   function($scope, $location, $route, Journey, Owner) {
     var regex = /users\/(\d*)/;
     var user_id = $location.absUrl().match(regex);
+    debugger
     // var user_id = regex.exec($location.absUrl());
     console.log("user id is: " + user_id);
     console.log(user_id[1])
-
+    debugger
     var owner = Owner.check( {id: user_id[1]} );
     owner.$promise.then( function(response) {
       $scope.isOwner = response.isOwner;
     });
-
+    debugger
     $scope.journeys = Journey.index({user_id: user_id[1]});
 
     $scope.showForm = false;
-
+    debugger
     $scope.displayForm = function() {
       $scope.showForm = true;
       $scope.journey = {};
     }
-
+    debugger
     $scope.createJourney = function() {
       Journey.create( $scope.journey ).$promise
         .then( function(response) {
           $location.url("/journeys/" + response.id );
         })
     }
-
+    debugger
     $scope.deleteJourney = function(journey) {
       var msg = "are you sure you want to delete this Journey and all the categories and notes in it?"
       if (confirm(msg)) {
